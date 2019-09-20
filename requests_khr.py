61680268ffb59afc5b12ebd5d55ed97bd806d226
SvKh99 <kris.alex.sveta@gmail.com>
2019-09-20 11:08:54 +0300

    create request

---
+++ requests_khr.py
@@ -0,0 +1,13 @@
+import requests
+import bs4
+
+
+
+url = "https://yandex.com/news/rubric/politics?from=index"
+
+yandex_politics_request = requests.get(url)
+yandex_politics_content = BeatifulSoup()
+
+
+
+print(yandex_politics_request)
